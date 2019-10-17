f6ac85740b5950db9f5f649fcea65e8a7fa25f6a
elenapan <lolotafrantzi@gmail.com>
2019-10-17 21:50:25 +0300

    Added configuration for deployment to Ropsten

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,10 @@ var GiftCards = artifacts.require("./GiftCards.sol");
 module.exports = function(deployer, network, accounts) {
   // TODO: replace dummy addresses
   const addressOwner = accounts[0];
-  const addressDAI = accounts[9];
-  deployer.deploy(GiftCards, addressOwner, addressDAI);
+
+  // Ropsten addresses
+  const addressDAI = "0xad6d458402f60fd3bd25163575031acdce07538d";
+  const kyberNetworkProxyAddress = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755";
+
+  deployer.deploy(GiftCards, addressOwner, addressDAI, kyberNetworkProxyAddress);
 };
