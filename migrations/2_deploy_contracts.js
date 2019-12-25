4146fa4693092c3860a99652973d264c84901c53
elenapan <lolotafrantzi@gmail.com>
2019-12-25 08:02:54 +0200

    Deployment: DAI, Kyber and maintainer addresses automatically determined based on network.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,32 @@
-var GiftCards = artifacts.require("./GiftCards.sol");
+const GiftCards = artifacts.require("./GiftCards.sol");
+
+// Addresses of external contracts (DAI, Kyber) as well as maintainer address
+// needed in order to deploy our GiftCards contract on Ropsten and Mainnet
+const ADDRESSES = {
+  // >>> Main net
+  1: {
+    addressDAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    addressKyberNetworkProxy: "0x818E6FECD516Ecc3849DAf6845e3EC868087B755",
+    // TODO get this from env var instead of hardcoding
+    addressMaintainer: "0xbd2912fe2b99f137867da4e8513fbb9fc6470698"
+  },
+  // >>> Ropsten
+  3: {
+    addressDAI: "0xad6d458402f60fd3bd25163575031acdce07538d",
+    addressKyberNetworkProxy: "0x818E6FECD516Ecc3849DAf6845e3EC868087B755",
+    addressMaintainer: null
+  }
+}
 
 module.exports = function(deployer, network, accounts) {
-  // TODO: replace dummy addresses
+  // Get external contract addresses automatically according to
+  // We use `deployer.network_id` instead of `network ` (network name) since
+  // truffle also deploys to fork networks (e.g. "mainnet-fork") which have the
+  // same network id as the original network but different network name
+  let { addressDAI, addressKyberNetworkProxy, addressMaintainer } = ADDRESSES[deployer.network_id];
+
   const addressOwner = accounts[0];
-  const addressMaintainer = "0xbd2912fe2b99f137867da4e8513fbb9fc6470698";
+  addressMaintainer = addressMaintainer ? addressMaintainer : accounts[0];
 
-  // Ropsten addresses
-  // const addressDAI = "0xad6d458402f60fd3bd25163575031acdce07538d";
-  // const kyberNetworkProxyAddress = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755";
-
-  // Mainnet addresses
-  const addressDAI = "0x6b175474e89094c44da98b954eedeac495271d0f";
-  const kyberNetworkProxyAddress = "0x818e6fecd516ecc3849daf6845e3ec868087b755";
-
-  deployer.deploy(GiftCards, addressOwner, addressMaintainer, addressDAI, kyberNetworkProxyAddress);
+  deployer.deploy(GiftCards, addressOwner, addressMaintainer, addressDAI, addressKyberNetworkProxy);
 };
