14f33ff5737e05aae969ece31a05a9aace59af85
elenapan <lolotafrantzi@gmail.com>
2019-12-26 12:06:28 +0200

    Deployment: Hardcode maintainer address for all networks

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,17 @@
 const GiftCards = artifacts.require("./GiftCards.sol");
 
-// Addresses of external contracts (DAI, Kyber) as well as maintainer address
-// needed in order to deploy our GiftCards contract on Ropsten and Mainnet
+// Addresses of external contracts (DAI, Kyber) needed in order to deploy our
+// GiftCards contract on Ropsten and Mainnet.
 const ADDRESSES = {
   // >>> Main net
   1: {
     addressDAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    addressKyberNetworkProxy: "0x818E6FECD516Ecc3849DAf6845e3EC868087B755",
-    // TODO get this from env var instead of hardcoding
-    addressMaintainer: "0xbd2912fe2b99f137867da4e8513fbb9fc6470698"
+    addressKyberNetworkProxy: "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
   },
   // >>> Ropsten
   3: {
     addressDAI: "0xad6d458402f60fd3bd25163575031acdce07538d",
-    addressKyberNetworkProxy: "0x818E6FECD516Ecc3849DAf6845e3EC868087B755",
-    addressMaintainer: null
+    addressKyberNetworkProxy: "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
   }
 }
 
@@ -23,10 +20,10 @@ module.exports = function(deployer, network, accounts) {
   // We use `deployer.network_id` instead of `network ` (network name) since
   // truffle also deploys to fork networks (e.g. "mainnet-fork") which have the
   // same network id as the original network but different network name
-  let { addressDAI, addressKyberNetworkProxy, addressMaintainer } = ADDRESSES[deployer.network_id];
+  let { addressDAI, addressKyberNetworkProxy } = ADDRESSES[deployer.network_id];
 
   const addressOwner = accounts[0];
-  addressMaintainer = addressMaintainer ? addressMaintainer : accounts[0];
+  const addressMaintainer = "0xbd2912fe2b99f137867da4e8513fbb9fc6470698";
 
   deployer.deploy(GiftCards, addressOwner, addressMaintainer, addressDAI, addressKyberNetworkProxy);
 };
